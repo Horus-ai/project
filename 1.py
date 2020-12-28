cf014d23ac54f89ca3b5847c3874701dc9ebe8df
Oleg <danrsoe95@mail.ru>
2020-12-28 22:33:27 +0300

    homework

---
+++ 1.py
@@ -0,0 +1,46 @@
+"""
+Задание 1.
+
+Докажите, что словари обрабатываются быстрее, чем списки.
+
+Реализуйте две функции, в первой нужно заполнить элементами список, во второй-словарь
+Сделайте замеры времени выполнения каждой из функций
+
+Подсказка: для замеров воспользуйтесь модулем time (см. примеры урока 1)
+
+Примечание: eсли вы уже знаете, что такое декоратор и как его реализовать,
+то реализуйте ф-цию-декоратор и пусть она считает время
+И примените ее к двум своим функциям.
+"""
+
+import time
+
+def time_it(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        func(args[0])
+        print(time.time() - start_time)
+    return wrapper
+
+# test_list = time_it(test_list)
+
+@time_it
+def test_list(n):
+    list_obj = []
+    for i in range(n):
+        list_obj.append(i)
+        #list_obj.index(j)
+    return list_obj
+
+@time_it
+def test_dict(n):
+    dict_obj = dict()
+    for i in range(n):
+        dict_obj[i] = i
+        # dict_obj.index(j)
+    return dict_obj
+
+
+test_list(100000)
+test_dict(100000)
+
